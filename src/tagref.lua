f7fbbb934274ddc977836520c5ebdd5bb41d3dcd
Christophe Delord <cdelord.fr>
2023-09-06 06:58:43 +0200

    build tagref with bang and ninja instead of make

--- src/tagref.lua
+++ src/tagref.lua
@@ -1,5 +1,23 @@
 #!/usr/bin/env luax
 
+-- This file is part of Tagref.
+--
+-- Tagref is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Tagref is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Tagref.  If not, see <https://www.gnu.org/licenses/>.
+--
+-- For further information about Tagref you can visit
+-- http://cdelord.fr/tagref
+
 local check, list_refs, list_tags, list_unused
 
 local function handle_args()
